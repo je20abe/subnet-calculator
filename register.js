328ddee909124ed2bb7f045616d098e012c839b5
Justice Eshun <je20abe@herts.ac.uk>
2025-01-16 20:11:40 +0000

    this is a code lines for my register.js to inititate a connection with my firebase

---
+++ register.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBhbByXlK57jzRW9GwCbTdX5wi3QmyWupY",
+  authDomain: "subnet-calculator-a0fe9.firebaseapp.com",
+  projectId: "subnet-calculator-a0fe9",
+  storageBucket: "subnet-calculator-a0fe9.firebasestorage.app",
+  messagingSenderId: "28241838449",
+  appId: "1:28241838449:web:53cc3cc885f6fd713ef6d0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
