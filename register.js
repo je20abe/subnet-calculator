0e845b2670f12a1adff30a89d4d19dd70adc9b45
Justice Eshun <je20abe@herts.ac.uk>
2025-01-23 09:59:15 +0000

    (firebase-auth): add Firebase app initialization and user authentication setup
    Initialized Firebase app using Firebase SDK.
    
    Implemented createUserWithEmailAndPassword for user registration.
    
    Included necessary Firebase SDKs for app and authentication.
    
    Also removed name, username, repeat-password form the signup.html to make implementation easier but will add it later on when everything is done

---
+++ register.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
+//import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBhbByXlK57jzRW9GwCbTdX5wi3QmyWupY",
+    authDomain: "subnet-calculator-a0fe9.firebaseapp.com",
+    projectId: "subnet-calculator-a0fe9",
+    storageBucket: "subnet-calculator-a0fe9.firebasestorage.app",
+    messagingSenderId: "28241838449",
+    appId: "1:28241838449:web:53cc3cc885f6fd713ef6d0"
+};
+
+
+
